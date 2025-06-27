567136cd1c3179f4ad046750aba1bf27a0f08488
thirstyice <tauranwood@gmail.com>
2025-06-27 15:51:23 -0600

    Combine duplicate code in Ether headers

--- ArtnetEther.h
+++ ArtnetEther.h
@@ -7,8 +7,11 @@
 #include <Arduino.h>
 #include <ArxTypeTraits.h>
 #include <ArxContainer.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
+#ifdef ARTNET_NATIVE_ETHER_H
+	#include <NativeEthernetUdp.h>
+#else
+	#include <EthernetUdp.h>
+#endif
 #include "Artnet/util/TeensyDirtySTLErrorSolution/TeensyDirtySTLErrorSolution.h"
 #include "Artnet/Manager.h"
 
--- ArtnetNativeEther.h
+++ ArtnetNativeEther.h
@@ -2,18 +2,6 @@
 #ifndef ARTNET_NATIVE_ETHER_H
 #define ARTNET_NATIVE_ETHER_H
 
-#define ARTNET_ENABLE_ETHER
-
-#include <Arduino.h>
-#include <ArxTypeTraits.h>
-#include <ArxContainer.h>
-#include <NativeEthernet.h>
-#include <NativeEthernetUdp.h>
-#include "Artnet/util/TeensyDirtySTLErrorSolution/TeensyDirtySTLErrorSolution.h"
-#include "Artnet/Manager.h"
-
-using Artnet = art_net::Manager<EthernetUDP>;
-using ArtnetSender = art_net::Sender<EthernetUDP>;
-using ArtnetReceiver = art_net::Receiver<EthernetUDP>;
+#include "ArtnetEther,h"
 
 #endif  // ARTNET_NATIVE_ETHER_H
